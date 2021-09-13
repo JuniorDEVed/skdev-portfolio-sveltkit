fc3daa27cc3a274830aa893b378306d0b09edf3c
Sean Knowles <s@seanknowles.dev>
2021-09-13 16:34:13 +0100

    cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
 	import { toggleService } from '/src/machines/toggleMachine.js';
-
 	$: toggleState = $toggleService.matches('inactive') ? 'Off' : 'On';
 
 	const toggle = () => {
@@ -14,14 +12,8 @@
 </script>
 
 <section>
-	<h1>
-		TESTING TESTING
-		<button on:click={toggle}>Switch</button>
-		{toggleState}
-	</h1>
-	<div>
-		<Counter />
-	</div>
+	<h1>Sean's Portfolio</h1>
+	<h3>Comming soon</h3>
 </section>
 
 <style>
